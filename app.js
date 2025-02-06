b80fef9ec2fa554ee3ad0e91e575036766780dca
JITHINRAJA <jithinraja@techfriar.com>
2025-02-07 00:51:16 +0530

    handling routes using express

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ const express= require('express');
 
 const app=express();
 
-app.use((req, res, next)=>{
-    console.log("in the middleware");
+app.use('/',(req, res, next)=>{
+    console.log("Always Works");
     next();
 });
 
-app.use((req, res, next)=>{
+app.use('/add-product',(req, res, next)=>{
+    console.log("in another middleware");
+    res.send('<h1>Add Product Page</h1>')
+});
+
+
+app.use('/',(req, res, next)=>{
     console.log("in another middleware");
     res.send('<h1>Hello From Express!</h1>')
 });
