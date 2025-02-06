674e7fae142def810b75be23ee8a4b5fb1c2dabe
JITHINRAJA <jithinraja@techfriar.com>
2025-02-07 00:38:27 +0530

    first

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const express= require('express');
+
+
+const app=express();
+
+app.use((req, res, next)=>{
+    console.log("in the middleware");
+    next();
+});
+
+app.use((req, res, next)=>{
+    console.log("in another middleware");
+    res.send('<h1>Hello From Express!</h1>')
+});
+
+app.listen(3000);
