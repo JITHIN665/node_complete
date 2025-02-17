9d6e79597619df6272aa8fafaac0b13ab609383c
JITHIN665 <jithinraja4@gmail.com>
2025-02-17 23:41:05 +0530

    add comment

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,13 @@ const express=require("express");
 
 const router=express.Router();
 
-
+/////add-product' GET Request
 router.get('/add-product',(req, res, next)=>{
     console.log("in another middleware");
     res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 });
 
-
+/////add-product' POST Request
 router.post('/add-product',(req, res, next)=>{
     console.log(req.body);
     console.log(req.body);
