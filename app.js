24a7581c66a20c7607b7b6c73f38a90e1b4da992
JITHINRAJA <jithinraja@techfriar.com>
2025-02-17 23:36:16 +0530

    routes

--- app.js
+++ app.js
@@ -5,17 +5,22 @@ const express= require('express');
 const adminRoutes=require('./routes/admin')
 const shopRoutes=require('./routes/shop')
 
-const bodyParser= require("body-parser")
-const app=express();
+const bodyParser= require("body-parser");
 
+
+
+
+const app=express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 
 app.use(shopRoutes);
 
-
+app.use((req,res,next)=>{
+   res.status(404).send("<h1>Page Not Found</h1>")
+})
 
 
 app.listen(3000);
