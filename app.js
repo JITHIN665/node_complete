7a3830fa237b521b44c7f674923867739395e355
JITHIN665 <jithinraja4@gmail.com>
2025-03-11 02:56:52 +0530

    session and cookies

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
+const MONGODB_URI =
+  'mongodb+srv://jithinraja4:Jithin%4099@cluster0.fpop2.mongodb.net/shops?retryWrites=true&w=majority&appName=Cluster0';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,23 +27,26 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById('67ce6b82875473a2c1828203')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
-      if (!user) {
-        console.log("User not found");
-        return next(); 
-      }
       req.user = user;
       next();
     })
-    .catch(err => {
-      console.error("Error fetching user:", err);
-      next(err); 
-    });
+    .catch(err => console.log(err));
 });
-
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -43,27 +54,23 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect("mongodb+srv://jithinraja4:Jithin%4099@cluster0.fpop2.mongodb.net/shops?retryWrites=true&w=majority&appName=Cluster0"
-).then(result=>
-{
-   User.findOne().then(
-    user=>{
-      if(!user){
+mongoose
+  .connect(MONGODB_URI)
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
         const user = new User({
-          name:"Jithin",
-          email:"test@123.com",
-          cart:{
-            items:[]
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
           }
         });
         user.save();
       }
-    }
-  );
-  
-  console.log("connected");
-  app.listen(3000);
-}
-).catch(err=>{
-  console.log(err);
-})
+    });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,55 +1,47 @@
-const Product = require("../models/product");
-const Order = require("../models/order");
+const Product = require('../models/product');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
-    .then((products) => {
-      res.render("shop/product-list", {
+    .then(products => {
+      console.log(products);
+      res.render('shop/product-list', {
         prods: products,
-        pageTitle: "All Products",
-        path: "/products",
-        isAuthenticated: req.isLoggedIn,
+        pageTitle: 'All Products',
+        path: '/products',
+        isAuthenticated: req.session.isLoggedIn
       });
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
     });
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  // Product.findAll({ where: { id: prodId } })
-  //   .then(products => {
-  //     res.render('shop/product-detail', {
-  //       product: products[0],
-  //       pageTitle: products[0].title,
-  //       path: '/products'
-  //     });
-  //   })
-  //   .catch(err => console.log(err));
   Product.findById(prodId)
-    .then((product) => {
-      res.render("shop/product-detail", {
+    .then(product => {
+      res.render('shop/product-detail', {
         product: product,
         pageTitle: product.title,
-        path: "/products",
-        isAuthenticated: req.isLoggedIn,
+        path: '/products',
+        isAuthenticated: req.session.isLoggedIn
       });
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
   Product.find()
-    .then((products) => {
-      res.render("shop/index", {
+    .then(products => {
+      res.render('shop/index', {
         prods: products,
-        pageTitle: "Shop",
-        path: "/",
-        isAuthenticated: req.isLoggedIn,
+        pageTitle: 'Shop',
+        path: '/',
+        isAuthenticated: req.session.isLoggedIn
       });
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
     });
 };
@@ -75,12 +67,12 @@ exports.getCart = async (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
-    .then((product) => {
+    .then(product => {
       return req.user.addToCart(product);
     })
-    .then((result) => {
+    .then(result => {
       console.log(result);
-      res.redirect("/cart");
+      res.redirect('/cart');
     });
 };
 
@@ -88,10 +80,10 @@ exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
     .removeFromCart(prodId)
-    .then((result) => {
-      res.redirect("/cart");
+    .then(result => {
+      res.redirect('/cart');
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
 
 exports.postOrder = async (req, res, next) => {
@@ -104,30 +96,30 @@ exports.postOrder = async (req, res, next) => {
       const order = new Order({
         user: {
           name: req.user.name,
-          userId: req.user,
+          userId: req.user
         },
-        products: products,
+        products: products
       });
       return order.save();
     })
-    .then((result) => {
+    .then(result => {
       return req.user.clearCart();
     })
     .then(() => {
-      res.redirect("/orders");
+      res.redirect('/orders');
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  Order.find({ "user.userId": req.user._id })
-    .then((orders) => {
-      res.render("shop/orders", {
-        path: "/orders",
-        pageTitle: "Your Orders",
+  Order.find({ 'user.userId': req.user._id })
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
         orders: orders,
-        isAuthenticated: req.isLoggedIn,
+        isAuthenticated: req.session.isLoggedIn
       });
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
