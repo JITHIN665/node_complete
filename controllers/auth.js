4d85f30044766f449e47b51089c24a7dfec97b36
JITHIN665 <jithinraja4@gmail.com>
2025-03-10 20:46:52 +0530

    cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,13 @@
 exports.getLogin = (req, res, next) => {
-  {
-    res.render("auth/login", {
-      path: "/login",
-      pageTitle: "Login",
-      isAuthenticated: req.isLoggedIn,
-    });
-  }
+  const isLoggedIn = req.get('Cookie').split(';')[0].trim().split('=')[1] === 'true';
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: isLoggedIn
+  });
 };
 
-
 exports.postLogin = (req, res, next) => {
-    res.setHeaders("Set-cookie", "loggedIn=true");
-    res.redirect("/"); 
-  };
+  res.setHeader('Set-Cookie', 'loggedIn=true');
+  res.redirect('/');
+};
