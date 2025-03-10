04e7c01a998c132d718b341d20afd70e6bf2855d
JITHIN665 <jithinraja4@gmail.com>
2025-03-10 17:35:04 +0530

    Finished the mongoose module

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
@@ -19,19 +20,48 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('67c9e4a48ca4cec939276694')
+  User.findById('67ce6b82875473a2c1828203')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      if (!user) {
+        console.log("User not found");
+        return next(); 
+      }
+      req.user = user;
       next();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.error("Error fetching user:", err);
+      next(err); 
+    });
 });
+
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose.connect("mongodb+srv://jithinraja4:Jithin%4099@cluster0.fpop2.mongodb.net/shops?retryWrites=true&w=majority&appName=Cluster0"
+).then(result=>
+{
+   User.findOne().then(
+    user=>{
+      if(!user){
+        const user = new User({
+          name:"Jithin",
+          email:"test@123.com",
+          cart:{
+            items:[]
+          }
+        });
+        user.save();
+      }
+    }
+  );
+  
+  console.log("connected");
   app.listen(3000);
-});
+}
+).catch(err=>{
+  console.log(err);
+})
