adfb00fc663a7974454a58dfe3344dcaa95b0694
JITHIN665 <jithinraja4@gmail.com>
2025-03-14 00:51:09 +0530

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { check, body } = require('express-validator');
 
-const authController = require('../controllers/auth');
+const authController = require('../controllers/aut');
 const User = require('../models/user');
 
 const router = express.Router();
